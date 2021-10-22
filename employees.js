eab07ebfb5b37740b7aaec59750e928c374ca0bb
Timothy Luebke <luebketj@gmail.com>
2021-10-22 10:46:41 -0600

    finsished the employees file

--- employees.js
+++ employees.js
@@ -20,7 +20,15 @@
 */
 
 //CODE HERE
-
+class Employee{
+    constructor(name, shifts){
+        this.name = name;
+        this.shifts = shifts;
+    }
+    getSchedule(){
+        console.log(`${this.name} works on ${this.shifts}`);
+    }
+}
 
 
 /*
@@ -34,14 +42,14 @@
 */
 
 //CODE HERE
-
+let empOne = new Employee("Jess", ["weekday mornings", "weekday afternoons"]);
 /*
     Call the `getSchedule` method on the
     `empOne` object.
 */
 
 //CODE HERE
-
+empOne.getSchedule();
 
 /*
     Make a copy of the empOne object
@@ -56,6 +64,8 @@
 */
 
 //CODE HERE
+let empTwo = {...empOne};
+empTwo.name = 'Nick';
 
 
 
@@ -83,7 +93,18 @@
 */
 
 //CODE HERE
-
+class Manager extends Employee{
+    constructor(name,shifts,employees){
+        super(name,shifts);
+        this.employees = employees;
+    }
+    getEmployees(){
+        console.log(`${this.name} manages ${this.employees}`);
+    }
+    addEmployee(emp){
+        this.employees.push(emp);
+    }
+}
 
 
 /*
@@ -98,7 +119,7 @@
 */
 
 //CODE HERE
-
+let manager = new Manager("Winston",["weekday mornings", "weekday afternoons"], ["Cece", "Schmidt"]);
 
 /*
     Call the `getEmployees` method on the
@@ -106,7 +127,7 @@
 */
 
 //CODE HERE
-
+manager.getEmployees();
 /*
     Call the `addEmployee` method on the 
     `manager` object passing in the string 
@@ -114,7 +135,7 @@
 */
 
 //CODE HERE 
-
+manager.addEmployee("Coach");
 /*
     Call the `getEmployees` method on the
     `manager` object again to confirm 
@@ -122,3 +143,4 @@
 */
 
 //CODE HERE
+manager.getEmployees();
